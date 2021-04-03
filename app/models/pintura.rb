17cb6050fea72f158d0bafb342b26e733887c348
Martin Bettaglio <martin.bettaglio@gmail.com>
2021-04-03 20:43:29 +0200

    home footer

--- app/models/pintura.rb
+++ app/models/pintura.rb
@@ -1,3 +1,5 @@
 class Pintura < ApplicationRecord
-CATEGORIES = ['Opus', 'El silencio del vacio'].freeze
+  CATEGORIES = ['Opus', 'El silencio del vacio'].freeze
+  has_one_attached :photo
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,88 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Pintura.destroy_all
+
+# require 'nokogiri'
+# require 'open-uri'
+
+  # file = URI.open('https://res.cloudinary.com/djrxx3xla/image/upload/v1616679278/opus_XII_zn4qgu.jpg')
+  # pintura = Pintura.new(title: "OPUS XII", detail: "Técnica mixta sobre chapa de madera y hierro", category: "Opus", size: "130 x 130 cm", year:"2000")
+  # pintura.photo.attach(io: file, filename: "opus_XII_zn4qgu.jpg", content_type: "img/jpg")
+
+#
+
+
+  # pintura = Pintura.new(title: "OPUS XII", detail: "Técnica mixta sobre chapa de madera y hierro", photo: URI.open('https://res.cloudinary.com/djrxx3xla/image/upload/v1616679278/opus_XII_zn4qgu.jpg'), category: "Opus", size: "130 x 130 cm", year:"2000")
+  # pintura.photo.attach(io: photo, filename: "opus_XII_zn4qgu.jpg", content_type: "img/jpg")
+
+
+
+# pintura = Pintura.create(
+#   title: "OPUS XIV",
+#   detail: "Técnica mixta sobre chapa de madera y hierro",
+#   photo: ,
+#   category: "Opus",
+#   size: "100 X 100 cm"
+#   year:"2000"
+#   )
+
+
+# pintura = Pintura.create(
+#   title: "OPUS XIII",
+#   detail: "Técnica mixta sobre chapa de madera y hierro",
+#   photo: ,
+#   category: "Opus",
+#   size: "130 x 130 cm"
+#   year:"2000"
+#   )
+
+
+# pintura = Pintura.create(
+#   title: "OPUS XV",
+#   detail: "Técnica mixta sobre chapa de madera y hierro",
+#   photo: ,
+#   category: "Opus",
+#   size: "180 X 180 cm"
+#   year:"2000"
+#   )
+
+
+# pintura = Pintura.create(
+#   title: "OPUS VII",
+#   detail: "Técnica mixta sobre chapa de madera y hierro",
+#   photo: ,
+#   category: "Opus",
+#   size: "40 X 40 cm"
+#   year:"2000"
+#   )
+
+
+# pintura = Pintura.create(
+#   title: "OPUS VIII",
+#   detail: "Técnica mixta sobre chapa de madera y hierro",
+#   photo: ,
+#   category: "Opus",
+#   size: "40 X 40 cm"
+#   year:"2000"
+#   )
+
+
+# pintura = Pintura.create(
+#   title: "OPUS X",
+#   detail: "Técnica mixta sobre chapa de madera y hierro",
+#   photo: ,
+#   category: "Opus",
+#   size: "100 X 100 cm"
+#   year:"2000"
+#   )
+
+
+# pintura = Pintura.create(
+#   title: "OPUS XX",
+#   detail: "Técnica mixta sobre chapa de madera y hierro",
+#   photo: ,
+#   category: "Opus",
+#   size: "100 X 100 cm"
+#   year:"2000"
+#   )
